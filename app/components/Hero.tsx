69ed195f4e0279e03612a849e0c7ad0e7740c74c
mark18cosmic <kaish018@gmail.com>
2024-10-18 12:12:44 +0500

    ANIMATIONS

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 import { Button } from "@/components/ui/button";
 
 const Hero = () => {
+  const heroTitleRef = useRef(null);
+  const heroSubtitleRef = useRef(null);
+  const heroButtonRef = useRef(null);
+
+  useEffect(() => {
+    // Animation timeline
+    const tl = gsap.timeline({ defaults: { ease: "power3.in", duration: 1 } });
+
+    tl.fromTo(
+      heroTitleRef.current,
+      { opacity: 0, y: 50 },
+      { opacity: 1, y: 0, duration: 1, }
+    )
+      .fromTo(
+        heroSubtitleRef.current,
+        { opacity: 0, y: 50 },
+        { opacity: 1, y: 0, duration: 2 },
+        "-=0.5"
+      )
+      .fromTo(
+        heroButtonRef.current,
+        { opacity: 0, y: 40 },
+        { opacity: 1, y: 0, duration: 1 },
+      );
+  }, []);
+
   return (
     <div
       className="relative w-full h-[100vh] bg-cover bg-center flex items-center justify-center"
@@ -15,17 +44,24 @@ const Hero = () => {
       <div className="absolute inset-0 bg-black opacity-60"></div>
 
       {/* Hero Content */}
-      <div className="relative z-10 flex flex-col items-center justify-center text-center text-white px-4 space-y-6 animate-fade-in-up">
-        <h1 className="text-5xl md:text-6xl lg:text-7xl font-extrabold tracking-wide drop-shadow-2xl">
+      <div className="relative z-10 flex flex-col items-center justify-center text-center text-white px-4 space-y-6">
+        <h1
+          ref={heroTitleRef}
+          className="text-5xl md:text-6xl lg:text-7xl font-extrabold tracking-wide drop-shadow-2xl"
+        >
           The Edge
         </h1>
-        <p className="mt-4 text-xl md:text-2xl lg:text-3xl font-medium text-gray-200 tracking-wide drop-shadow-lg">
+        <p
+          ref={heroSubtitleRef}
+          className="mt-4 text-xl md:text-2xl lg:text-3xl font-medium text-gray-200 tracking-wide drop-shadow-lg"
+        >
           Empowering Excellence in the Hospitality Industry
         </p>
 
         {/* Optional Call to Action Button */}
         <Button
-          className="mt-8 px-8 py-4 text-lg font-bold text-black bg-white hover:bg-gray-200 rounded-full shadow-lg transition duration-300 ease-in-out transform hover:scale-105"
+          ref={heroButtonRef}
+          className="mt-8 px-8 py-4 text-lg font-bold text-black bg-white hover:bg-gray-200 rounded-xl shadow-lg transition duration-300 ease-in-out transform hover:scale-105"
           aria-label="Learn More"
         >
           Learn More
